1e3f1524b83dc66f99f8687f85e4a1296f2abebf
RaynGoslenko <olkhovatyi@knu.ua>
2023-03-14 09:40:12 +0200

    Edited scrapper.py

--- scrapper.py
+++ scrapper.py
@@ -11,22 +11,24 @@ def clone_repos(path, save_dir='resources'):
         call(['git', 'clone', line.strip(), walking_dir])
 
 
-def find_files(path, save_dir='resources', extension='.py'):
+def find_files(path, save_dir='resources', extensions=None):
+    if extensions is None:
+        extensions = ['.py']
     with open(path, 'r') as f:
         lines = f.readlines()
-    total_files = []
+    total_files_path = []
     for line in lines:
         walking_dir = f'{save_dir}/{line.strip().split("/")[-1]}'
         for (current_path, folders, files) in os.walk(walking_dir):
             for file in files:
-                if file[-3:] == extension:
-                    total_files.append(os.path.join(current_path, file))
-    return total_files
+                if file[-3:] in extensions:
+                    total_files_path.append(os.path.join(current_path, file))
+    return total_files_path
 
 
-def jsonify(files, bos_token='<BOS>', eos_token='<EOS>'):
+def jsonify(files_path, bos_token='<BOS>', eos_token='<EOS>'):
     json = []
-    for file in files:
+    for file in files_path:
         with open(file, 'r') as f:
             try:
                 content = f.readlines()
@@ -50,6 +52,6 @@ def json_to_csv(json, path, fieldnames=None):
 
 
 clone_repos('repos.txt')
-files_list = find_files('repos.txt')
-json_data = jsonify(files_list)
+files_path_list = find_files('repos.txt')
+json_data = jsonify(files_path_list)
 json_to_csv(json_data, 'data/data.csv')
