a59d0ddcbb0b65ca71cada3c3350ebd82cb666da
RaynGoslenko <olkhovatyi@knu.ua>
2023-03-16 22:05:20 +0200

    Refactored scrapper. Now it is a class with the same functionality

--- scrapper.py
+++ scrapper.py
@@ -3,61 +3,68 @@ import csv
 from subprocess import call
 
 
-def clone_repos(path, save_dir='resources'):
-    with open(path, 'r') as f:
+def clone_repos(repos, clone_folder):
+    with open(repos, 'r') as f:
         lines = f.readlines()
     for line in lines:
-        walking_dir = f'{save_dir}/{"/".join(line.strip().split("/")[-2:])}'
+        walking_dir = f'{clone_folder}/{"/".join(line.strip().split("/")[-2:])}'
         call(['git', 'clone', line.strip(), walking_dir])
 
 
-def find_files(path, save_dir='resources', max_size=1000000, extensions=None):
-    if extensions is None:
-        extensions = ['py']
-    with open(path, 'r') as f:
-        lines = f.readlines()
-    total_files_path = []
-    for line in lines:
-        walking_dir = f'{save_dir}/{"/".join(line.strip().split("/")[-2:])}'
-        for (current_path, folders, files) in os.walk(walking_dir):
-            for file in files:
-                size = os.path.getsize(path)
-                # Only files with certain extensions and under max_size
-                if (file.split('.')[-1] in extensions) & (size < max_size):
-                    total_files_path.append(os.path.join(current_path, file))
-    return total_files_path
+class Scrapper:
+    def __init__(self, bos_token='<BOS>', eos_token='<EOS>'):
+        self.bos_token = bos_token
+        self.eos_token = eos_token
+        self.fieldnames = ['text', 'repo_name', 'path']
+        self.files_path = []
+        self.json = []
+
+    def scrap(self, repos, clone_folder='resources', max_file_size=1000000, extensions=None):
+        if extensions is None:
+            extensions = ['py']
+        clone_repos(repos, clone_folder)
+        self.__find_files(repos, clone_folder, max_file_size, extensions)
+        self.__jsonify()
+
+    def save(self, path):
+        self.__json_to_csv(path)
+
+    def __find_files(self, repos, clone_folder, max_file_size, extensions):
+        with open(repos, 'r') as f:
+            lines = f.readlines()
+        for line in lines:
+            walking_dir = f'{clone_folder}/{"/".join(line.strip().split("/")[-2:])}'
+            for (current_path, folders, files) in os.walk(walking_dir):
+                for file in files:
+                    size = os.path.getsize(repos)
+                    # Only files with certain extensions and under max_size
+                    if (file.split('.')[-1] in extensions) & (size < max_file_size):
+                        self.files_path.append(os.path.join(current_path, file))
+
+    def __jsonify(self):
+        for path in self.files_path:
+            with open(path, 'r') as f:
+                try:
+                    content = f.readlines()
+                except UnicodeDecodeError:
+                    print('DecoderError: ', path)
+                summary = ''.join(content)
+                summary = str(summary).strip()
+                data = self.bos_token + summary + self.eos_token
+                repo_name = '/'.join(path.split('/')[1:3])
+                file_path = '/'.join(path.split('/')[3:])
+                self.json.append({self.fieldnames[0]: data,
+                                  self.fieldnames[1]: repo_name,
+                                  self.fieldnames[2]: file_path})
+
+    def __json_to_csv(self, path):
+        with open(path, 'w') as csv_file:
+            writer = csv.DictWriter(csv_file, fieldnames=self.fieldnames)
+            writer.writeheader()
+            for data in self.json:
+                writer.writerow(data)
 
 
-def jsonify(files_path, bos_token='<BOS>', eos_token='<EOS>'):
-    json = []
-    for path in files_path:
-        with open(path, 'r') as f:
-            try:
-                content = f.readlines()
-            except UnicodeDecodeError:
-                print('DecoderError: ', path)
-            summary = ''.join(content)
-            summary = str(summary).strip()
-            data = bos_token + summary + eos_token
-            repo_name = '/'.join(path.split('/')[1:3])
-            file_path = '/'.join(path.split('/')[3:])
-            json.append({'text': data,
-                         'repo_name': repo_name,
-                         'path': file_path})
-    return json
-
-
-def json_to_csv(json, path, fieldnames=None):
-    if fieldnames is None:
-        fieldnames = ['text', 'repo_name', 'path']
-    with open(path, 'w') as csv_file:
-        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-        writer.writeheader()
-        for data in json:
-            writer.writerow(data)
-
-
-clone_repos('repos.txt')
-files_path_list = find_files('repos.txt')
-json_data = jsonify(files_path_list)
-json_to_csv(json_data, 'data/data.csv')
+scrapper = Scrapper()
+scrapper.scrap('repos.txt')
+scrapper.save('data/data.csv')
